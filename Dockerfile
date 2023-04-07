52e99fabde514a69b644287ea91ffb6dbc2460c0
Antonin Jech <a.jech@audaris.de>
2023-04-07 20:34:27 +0200

    prisma and knex

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,9 @@ WORKDIR /usr/src/app
 COPY package.json yarn.lock ./
 RUN yarn install --frozen-lockfile || yarn install
 
+COPY prisma/schema.prisma ./prisma/
+RUN npx prisma generate
+
 COPY . .
 
 EXPOSE 8080
--- src/index.ts
+++ src/index.ts
@@ -6,11 +6,12 @@ app.use(morgan('dev'))
 
 app.get('/', (req, res) => {
     res.json({
-        hello: 'tonyleejonesii'
+        hello: 'tony'
     })
 })
 
 const port = Number(process.env.PORT || 8080)
+
 // 0.0.0.0 host is a must when we run the app in docker
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is listening on port ${port}`)
