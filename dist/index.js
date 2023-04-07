1e258289d16441ea12c22b33ad532a863b8b1412
Antonin Jech <a.jech@audaris.de>
2023-04-07 13:04:06 +0200

    added build script

---
+++ dist/index.js
@@ -0,0 +1,4 @@
+"use strict";
+console.log("Hello World");
+
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uL3NyYy9pbmRleC50cyJdLCJzb3VyY2VzQ29udGVudCI6WyJjb25zb2xlLmxvZygnSGVsbG8gV29ybGQnKSJdLCJuYW1lcyI6WyJjb25zb2xlIiwibG9nIl0sIm1hcHBpbmdzIjoiO0FBQUFBLFFBQVFDLEdBQUcsQ0FBQyJ9
